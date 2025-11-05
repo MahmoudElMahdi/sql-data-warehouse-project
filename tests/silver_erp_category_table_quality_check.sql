f3434bedd13c5e244bb7dd9966f0bdead3af436d
Mahmoud Mohamed Abdelaziz <61700122+MahmoudElMahdi@users.noreply.github.com>
2025-11-05 19:37:09 +0200

    Refine comments for quality check SQL script
    Updated comments to enhance clarity and detail about the script's purpose and usage.

--- tests/silver_erp_category_table_quality_check.sql
+++ tests/silver_erp_category_table_quality_check.sql
@@ -1,8 +1,19 @@
 /*
-	Data Checking and Cleansing Steps:
-  1- Check for unwanted Characters
-  2- Check for unwanted Spaces
-	3- Check Data Standardization & Consistency.
+	==================================================================================
+	Quality Checks
+	==================================================================================
+	Script Purpose:
+		This script performs various quality checks for data consistency, accuracy,
+	 	and standardization across the 'silver' schema. It includes checks for:
+		- Null or duplicated primary keys.
+		- Unwanted spaces in string fields.
+		- Data standardization and consistency.
+		- Invalid date ranges and orders.
+		- Data consistency between related fields.
+	Usage Notes:
+		- Run these checks after loading the Silver Layer.
+		- Investigate and resolve any discrepancies found during the checks.
+	==================================================================================
 */
 
 SELECT
